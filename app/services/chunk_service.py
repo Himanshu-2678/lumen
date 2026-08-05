2df93c17d5f6192e8bae927d985fafbca00d020f
Himanshu Yadav <168503095+Himanshu-2678@users.noreply.github.com>
2026-08-05 22:48:27 +0530

    implement service to store the chunks in chunks table

---
+++ app/services/chunk_service.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+
+from app.models.chunk import Chunk
+
+
+def save_chunks(
+    db: Session,
+    chunks: list[dict],
+):
+    db_chunks = []
+
+    for chunk in chunks:
+
+        db_chunk = Chunk(
+            document_id=chunk["document_id"],
+            filename=chunk["filename"],
+            page_number=chunk["page_number"],
+            chunk_index=chunk["chunk_index"],
+            text=chunk["text"],
+        )
+
+        db_chunks.append(db_chunk)
+
+    db.add_all(db_chunks)
+    db.commit()
+
+    return db_chunks
+
+
+def get_all_chunks(db: Session):
+    return db.query(Chunk).all()
